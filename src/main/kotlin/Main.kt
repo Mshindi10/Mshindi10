c14772fd84d107866a7c3dda1f6d32b523279143
Gideon Mshindi <104357958+Mshindi10@users.noreply.github.com>
2023-09-15 14:11:26 +0300

    complete except consonant

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-
 fun main(args: Array<String>) {
-    print("Enter a number: ")
-    val stringInput = readLine()!!
-    var integer:Int = stringInput.toInt()
-    println("You entered: $integer")
+    println("What is your name?")
+    val  name = readln()
+    println("Hello ,$name")
+
+        print("Enter a number: ")
+        val stringInput = readLine()!!
+        var integer:Int = stringInput.toInt()
+        println("You entered: $integer")
+
+        val first: Int = 10
+        val second: Int = 20
+        val sum = first + second
+        println("The sum is: $sum")
+
+    val ch = 'i'
+    val vowelConsonant = if (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u')"vowel" else "consonant"
+    println("$ch is $vowelConsonant")
+
+    val year = 1990
+    var leap = false
+
+    if (year % 4 ==0) {
+        if (year %100 == 0) {
+            leap = year % 400 == 0
+        }else
+            leap = true
+    }else
+        leap = false
+
+    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
 }
